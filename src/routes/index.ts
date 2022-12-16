56ad065775803c675c0b26764153ce39a2269023
Sabin Adams <sabintheworld@gmail.com>
2022-12-16 11:09:07 -0800

    consolidates middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,10 @@ const router = Router()
 
 import quote from './quote.route'
 import auth from './auth.route'
+import authorizationMiddleware from 'middlewares/authorization.middleware'
 
-router.use('/quote', quote)
 router.use('/auth', auth) 
+router.use('/quote', authorizationMiddleware, quote)
 
 router.all('*', ( req, res, next ) => {
 	res.status(404).json({
--- src/routes/quote.route.ts
+++ src/routes/quote.route.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
 const router = Router()
 import * as QuoteController from 'controllers/quote.controller'
-import authorizationMiddleware from 'middlewares/authorization.middleware'
 import validate from 'middlewares/validate.middleware'
 import { CreateQuoteSchema, DeleteQuoteSchema } from 'validation/request.schemas'
 
-router.get('/', authorizationMiddleware, QuoteController.getAllQuotes)
-router.post('/', authorizationMiddleware, validate(CreateQuoteSchema), QuoteController.createQuote)
-router.delete('/:id', authorizationMiddleware, validate(DeleteQuoteSchema), QuoteController.deleteQuote)
+router.get('/', QuoteController.getAllQuotes)
+router.post('/', validate(CreateQuoteSchema), QuoteController.createQuote)
+router.delete('/:id', validate(DeleteQuoteSchema), QuoteController.deleteQuote)
 
 export default router
