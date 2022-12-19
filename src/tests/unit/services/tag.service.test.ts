4ee6863fc357f85097f99d618362c03f5a674d78
Sabin Adams <sabintheworld@gmail.com>
2022-12-19 11:24:52 -0800

    Finishes tag service test

--- src/tests/unit/services/tag.service.test.ts
+++ src/tests/unit/services/tag.service.test.ts
@@ -112,4 +112,25 @@ describe('tag.service', () => {
       expect(prismaMock.$transaction).toHaveReturnedWith([])
     })
   })
+
+  describe('deleteOrphanedTags', () => {
+    it('should delete tags that are not associated with any quotes', async () => {
+      prismaMock.tag.deleteMany.mockResolvedValue({ count: 1 })
+
+      const result = await TagService.deleteOrphanedTags([1, 2, 3])
+      expect(result).toStrictEqual({ count: 1 })
+    })
+
+    it('should filter by tags that have a matching id and no quotes associated', async () => {
+      prismaMock.tag.deleteMany.mockResolvedValue({ count: 1 })
+      await TagService.deleteOrphanedTags([1, 2, 3])
+
+      expect(prismaMock.tag.deleteMany).toHaveBeenCalledWith({
+        where: {
+          quotes: { none: {} },
+          id: { in: [1, 2, 3] }
+        }
+      })
+    })
+  })
 })
