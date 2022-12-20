749278e30e571d15b6d036a2137e78cc551feb7d
Sabin Adams <sabintheworld@gmail.com>
2022-12-19 23:06:30 -0800

    Finishes tests for AppError class

--- src/tests/unit/lib/utility-classes.test.ts
+++ src/tests/unit/lib/utility-classes.test.ts
@@ -3,13 +3,18 @@ import { describe, expect, it } from 'vitest'
 
 describe('utility-classes', () => {
   describe('AppError', () => {
-    it('Should set the status code based on the type of error', () => {
+    it('should set the status code based on the type of error', () => {
       const error = new AppError('validation', 'validation-error')
       const error2 = new AppError('server', 'server-error')
       const error3 = new AppError('unauthorized', 'auth-error')
       expect(error.statusCode).toBe(400)
       expect(error2.statusCode).toBe(500)
       expect(error3.statusCode).toBe(401)
+    })
+
+    it('should set the error message', () => {
+      const error = new AppError('validation', 'validation-error')
+      expect(error.message).toBe('validation-error')
     })
   })
 })
