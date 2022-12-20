04f8e1cae626f391ab002c749e5ba5e9cba4f6eb
Sabin Adams <sabintheworld@gmail.com>
2022-12-19 23:12:44 -0800

    I'm OCD...

--- src/tests/unit/lib/utility-classes.test.ts
+++ src/tests/unit/lib/utility-classes.test.ts
@@ -4,12 +4,12 @@ import { describe, expect, it } from 'vitest'
 describe('utility-classes', () => {
   describe('AppError', () => {
     it('should set the status code based on the type of error', () => {
-      const error = new AppError('validation', 'validation-error')
-      const error2 = new AppError('server', 'server-error')
-      const error3 = new AppError('unauthorized', 'auth-error')
-      expect(error.statusCode).toBe(400)
-      expect(error2.statusCode).toBe(500)
-      expect(error3.statusCode).toBe(401)
+      const error1 = new AppError('validation', 'validation-error')
+      const error2 = new AppError('unauthorized', 'auth-error')
+      const error3 = new AppError('server', 'server-error')
+      expect(error1.statusCode).toBe(400)
+      expect(error2.statusCode).toBe(401)
+      expect(error3.statusCode).toBe(500)
     })
 
     it('should set the error message', () => {
