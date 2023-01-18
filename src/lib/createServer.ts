0f7fe247deb85833faf639b58024f77a019c77f3
Sabin Adams <sabintheworld@gmail.com>
2023-01-18 10:38:00 -0800

    Adds auth to quotes routes

--- src/lib/createServer.ts
+++ src/lib/createServer.ts
@@ -1,12 +1,13 @@
 import authRouter from 'auth/auth.router'
 import express from 'express'
 import { errorHandler } from 'lib/middlewares'
+import { authorization } from 'lib/middlewares'
 import quotesRouter from 'quotes/quotes.router'
 
 export default () => {
   const app = express()
   app.use(express.json())
-  app.use('/quotes', quotesRouter)
+  app.use('/quotes', authorization, quotesRouter)
   app.use('/auth', authRouter)
   app.use(errorHandler)
   return app
