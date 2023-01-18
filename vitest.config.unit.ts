81eea4076004e440668f18854074e7dd4c6fa188
Sabin Adams <sabintheworld@gmail.com>
2023-01-17 16:02:26 -0800

    format

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts']
+    include: ['src/**/*.test.ts', '!src/tests']
   },
   resolve: {
     alias: {
